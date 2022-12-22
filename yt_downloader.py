2164e1d2f5de326e1fcc5c4f859b984c9ecbf3f3
Filipe Guidastri <117661407+FiGuidastri@users.noreply.github.com>
2022-12-22 14:13:49 -0300

    Update yt_downloader.py

--- yt_downloader.py
+++ yt_downloader.py
@@ -2,7 +2,7 @@ from pytube import YouTube
 
 def download(link):
     youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_quality()
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
     try:
         youtubeObject.download()
     except:
